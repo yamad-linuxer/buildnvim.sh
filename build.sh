b7720932d6e5eba5b957517b16b16795086fb1e5
YamaD <yamad.linuxer@gmail.com>
2020-05-20 21:59:03 +0900

    動かないと思ったら僕が悪かった

--- build.sh
+++ build.sh
@@ -7,22 +7,9 @@ set -e
 
 SOURCE_DIR=${1:-./}
 
-BUILD_TMP_DIR=~/.nvim_builder_tmp
+BUILDER_TMP_DIR=~/.nvim_builder_tmp
 
-BUILD_DATE=`date +'%F'`
-
-OUT_DIR="/opt/neovim/${BUILD_DATE}"
-
-[[ -d /opt/neovim ]] || sudo mkdir -p /opt/neovim
-
-if [[ -d "${OUT_DIR}" ]]; then
-    for ((i=1;;i++)); do
-        if [[ ! -d "${OUT_DIR}_${i}" ]]; then
-            OUT_DIR="${OUT_DIR}_${i}"
-            break
-        fi
-    done
-fi
+OUT_DIR="/opt/neovim"
 
 putlog () {
     echo " -<[ INFO ]>- : ${@}"
@@ -33,27 +20,44 @@ die () {
     exit 1
 }
 
+BUILD_DATE=`date +'%F'`
+
+[[ -d "${OUT_DIR}" ]] || sudo mkdir -p "${OUT_DIR}"
+
+if [[ -d "${OUT_DIR}/neovim_${BUILD_DATE}" ]]; then
+    for ((i=1;;i++)); do
+        if [[ ! -d "${OUT_DIR}/neovim_${BUILD_DATE}_${i}" ]]; then
+            DIST_DIR="${OUT_DIR}/neovim_${BUILD_DATE}_${i}"
+            break
+        fi
+    done
+else
+    DIST_DIR="${OUT_DIR}/neovim_${BUILD_DATE}"
+fi
+
 
 #    ----< MAIN PROCESS >----
 
 
-[[ -d "${BUILD_TMP_DIR}" ]] && rm ${BUILD_TMP_DIR}
-cp -r ${SOURCE_DIR} ${BUILD_TMP_DIR}
+[[ -d "${BUILDER_TMP_DIR}" ]] && rm -rf ${BUILDER_TMP_DIR}
+cp -r ${SOURCE_DIR} ${BUILDER_TMP_DIR}
 
 [[ -d "${SOURCE_DIR}/.git" ]] || \
     die "Git repository was not found in ${SOURCE_DIR}."
 
+cd ${BUILDER_TMP_DIR}
+
 git pull
 putlog "Pulled the git repository."
 
-make -j5 CMAKE_INSTALL_PREFIX=${OUT_DIR}
+make -j5 CMAKE_INSTALL_PREFIX=${DIST_DIR}
 putlog "Build new NeoVim done."
 
 sudo make install
-putlog "Installing NeoVim to ${OUT_DIR} done."
+putlog "Installing NeoVim to ${DIST_DIR} done."
 
-sudo update-alternatives --install /usr/local/bin/nvim neovim ${OUT_DIR}/bin/nvim 101
+sudo update-alternatives --install /usr/local/bin/nvim neovim ${DIST_DIR}/bin/nvim 101
 sudo update-alternatives --display neovim
 putlog "Older nvim link was switched to new one."
 
-rm -rf ${BUILD_TMP_DIR}
+rm -rf ${BUILDER_TMP_DIR}
